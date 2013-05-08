c7c4aa5e9dce02fa6a54d0fcfe30bfb70dd5e64c
Tom De Vylder <tom@penumbra.be>
2013-05-08 14:12:41 +0200

    Add documentation

--- manifests/export.pp
+++ manifests/export.pp
@@ -1,12 +1,22 @@
 # = Define nfs::export
 #
+# === Parameters:
+#
+# $export_directory   Define which directory to export.
+#
+# $export_target      Define which target to export.
+#
+# $ensure             Default: present.
+#
+# $export_options     Define export options.
+#                     Default: 'rw,no_root_squash,no_subtree_check'.
+#
 define nfs::export (
   $export_directory,
   $export_target,
   $ensure         = present,
   $export_options = 'rw,no_root_squash,no_subtree_check',
 ) {
-
   if ! $::nfs::server {
     fail 'NFS server support is not enabled.'
   }
